cd6b959b0f16629c58f02317e6474dab4890b92a
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-06 16:46:12 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    // This function will be executed when the DOM is fully loaded.
+    const body = document.body;
+
+    // Create a div element to contain the message and append it to the body.
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = "DOM load success";
+    body.appendChild(messageDiv);
+});
